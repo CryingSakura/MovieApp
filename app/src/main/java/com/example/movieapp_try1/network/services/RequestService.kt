83b1111f7ee7dc0a38b8839a9ac3216dacda89f6
Danya Goga <CryingSakura69@gmail.com>
2024-02-12 21:43:00 +0200

    comment Query and Path anotation

--- app/src/main/java/com/example/movieapp_try1/network/services/RequestService.kt
+++ app/src/main/java/com/example/movieapp_try1/network/services/RequestService.kt
@@ -1,5 +1,20 @@
 package com.example.movieapp_try1.network.services
+import com.example.movieapp_try1.data.model.MovieModel
+import retrofit2.Response
+import retrofit2.http.GET
+
 
 interface RequestService {
+
+    @GET("movie")
+    suspend fun getMovieList(
+/*        @Query("api_key") apikey: String,
+        @Query("page") page: Int,
+        @Path("type") type: String*/
+    ): Response<MovieModel>
+
+    companion object{
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
 
 }
