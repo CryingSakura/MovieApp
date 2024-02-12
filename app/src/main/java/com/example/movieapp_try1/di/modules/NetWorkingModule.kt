8312021f9face628b89d9fcd1ce59fe520a7dfaf
Danya Goga <CryingSakura69@gmail.com>
2024-02-12 21:41:33 +0200

    add provides for retrofit

--- app/src/main/java/com/example/movieapp_try1/di/modules/NetWorkingModule.kt
+++ app/src/main/java/com/example/movieapp_try1/di/modules/NetWorkingModule.kt
@@ -1,9 +1,12 @@
 package com.example.movieapp_try1.di.modules
 
+import com.example.movieapp_try1.network.services.RequestService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 @Module
@@ -11,5 +14,14 @@ import dagger.hilt.components.SingletonComponent
 class NetWorkingModule {
 
 
+    @Provides
+    fun providesRetrofit(): Retrofit{
+        return Retrofit.Builder().baseUrl(RequestService.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    @Provides
+    fun providesRequestService(retrofit: Retrofit): RequestService{
+        return retrofit.create(RequestService::class.java)
+    }
 
 }
