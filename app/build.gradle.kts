48bf03808fcc5bd0277c5ab932fc737ae75b62ee
Danya Goga <CryingSakura69@gmail.com>
2024-02-13 18:21:06 +0200

    repair hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
 }
 
 android {
@@ -80,6 +80,7 @@ dependencies {
     //hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     //navigation
     val nav_version = "2.7.7"
