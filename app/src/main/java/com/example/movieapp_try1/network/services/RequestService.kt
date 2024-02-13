0687174cee66f4b0f794358179bf71c592cf9c30
Danya Goga <CryingSakura69@gmail.com>
2024-02-13 18:26:26 +0200

    add param for request api data, change returning data

--- app/src/main/java/com/example/movieapp_try1/network/services/RequestService.kt
+++ app/src/main/java/com/example/movieapp_try1/network/services/RequestService.kt
@@ -1,20 +1,29 @@
 package com.example.movieapp_try1.network.services
-import com.example.movieapp_try1.data.model.MovieModel
+import com.example.movieapp_try1.data.model.PopularMovieList
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface RequestService {
 
-    @GET("movie")
+    @GET("movie/popular")
     suspend fun getMovieList(
-/*        @Query("api_key") apikey: String,
+        /*@Path("movie_id") movie_id: Int,*/
         @Query("page") page: Int,
-        @Path("type") type: String*/
-    ): Response<MovieModel>
+        @Query("api_key") apikey: String
+
+
+/*        @Query("query") query: String,
+
+        */
+    ): Response<PopularMovieList>
 
     companion object{
         const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val API = "baa4c8f60cd56f6255a334b048ad6518"
     }
+
+
 
 }
