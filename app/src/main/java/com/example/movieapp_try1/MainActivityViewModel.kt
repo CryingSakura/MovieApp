a0b0fcf5267ae5c849e36f3bd48afd32c78fd124
Danya Goga <CryingSakura69@gmail.com>
2024-02-13 18:22:15 +0200

    add response for tests

--- app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.movieapp_try1
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.movieapp_try1.network.ReqRep
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -9,6 +12,11 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     private val requestRepository: ReqRep
 ): ViewModel() {
+
+    fun fetchMovieData() = viewModelScope.launch {
+        val movieResponse = requestRepository.getMovieList()
+        Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()}")
+    }
 
 
 
