7ad7e1c4e6cf5da6640f18adef065eaa5bcaa2b9
Danya Goga <CryingSakura69@gmail.com>
2024-02-13 18:21:40 +0200

    add space?

--- app/src/main/java/com/example/movieapp_try1/MainActivity.kt
+++ app/src/main/java/com/example/movieapp_try1/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.movieapp_try1.screens.MainView
 import com.example.movieapp_try1.screens.navigation.RootNavigationGraph
 import com.example.movieapp_try1.ui.theme.MovieApp_try1Theme
 import com.example.movieapp_try1.ui.theme.RaisinBlack
@@ -16,6 +15,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
