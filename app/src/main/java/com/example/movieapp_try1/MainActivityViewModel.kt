c732b1149c83be00427a6e792fd7784ce8c10fca
Danya Goga <CryingSakura69@gmail.com>
2024-02-15 15:10:03 +0200

    impl Flow for getting urlimg to card

--- app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieapp_try1.network.ReqRep
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,10 +16,25 @@ class MainActivityViewModel @Inject constructor(
     private val requestRepository: ReqRep
 ): ViewModel() {
 
+    private val _state = MutableStateFlow(Try())
+    val state = _state.asStateFlow()
+
+
+
+
+
     fun fetchMovieData() = viewModelScope.launch {
         val movieResponse = requestRepository.getMovieList()
+        _state.update { it.copy(urlImg = requestRepository.getMovieList().body()!!.results[1].backdrop_path) }
         Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()}")
     }
+
+
+
+
+
+
+
 
 
 
