b300dd11dd59b9c02b0d868b2ecc3b16508b056e
Danya Goga <CryingSakura69@gmail.com>
2024-02-26 15:05:01 +0200

    create flow pagdatalist of items from room db

--- app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
@@ -1,33 +1,56 @@
 package com.example.movieapp_try1
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.movieapp_try1.network.ReqRep
+import androidx.paging.Pager
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.example.movieapp_try1.data.local.ResultsEntity
+import com.example.movieapp_try1.data.mappers.toResult
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    private val requestRepository: ReqRep
+    pager: Pager<Int, ResultsEntity>,
 ): ViewModel() {
 
-    private val _state = MutableStateFlow(Try())
-    val state = _state.asStateFlow()
+
+
+    val moviePagingFlow = pager
+        .flow
+        .map {pagingData ->
+            pagingData.map { it.toResult() }
+        }
+        .cachedIn(viewModelScope)
 
 
 
 
 
-    fun fetchMovieData() = viewModelScope.launch {
+
+
+/*
+
+    private val _stateResult = MutableStateFlow(Result())
+    val stateResult = _stateResult.asStateFlow()
+
+    private val _statePopList = MutableStateFlow(PopularMovieList())
+    val statePopList = _statePopList.asStateFlow()
+*/
+
+
+
+
+
+    /*fun fetchMovieData() = viewModelScope.launch {
         val movieResponse = requestRepository.getMovieList()
-        _state.update { it.copy(urlImg = requestRepository.getMovieList().body()!!.results[1].backdrop_path) }
+        *//*_stateResult.update { it.copy(backdrop_path = requestRepository.getMovieList().body()!!.results[0].poster_path) }*//*
+        _statePopList.update { it.copy(results = requestRepository.getMovieList().body()!!.results) }
         Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()}")
-    }
+        Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()!!.results.size}")
+    }*/
 
 
 
