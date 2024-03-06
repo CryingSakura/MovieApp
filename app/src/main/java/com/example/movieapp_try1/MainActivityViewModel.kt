04a0447df42862c43c70c3d4c8bf58b3bc405672
Danya Goga <CryingSakura69@gmail.com>
2024-03-06 18:17:43 +0200

    add MutableStateFlow and function for storage data in flow and get this to detailScreen

--- app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
+++ app/src/main/java/com/example/movieapp_try1/MainActivityViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.movieapp_try1
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -6,8 +7,12 @@ import androidx.paging.cachedIn
 import androidx.paging.map
 import com.example.movieapp_try1.data.local.ResultsEntity
 import com.example.movieapp_try1.data.mappers.toResult
+import com.example.movieapp_try1.data.model.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 
@@ -31,26 +36,39 @@ class MainActivityViewModel @Inject constructor(
 
 
 
-/*
+
 
     private val _stateResult = MutableStateFlow(Result())
     val stateResult = _stateResult.asStateFlow()
 
-    private val _statePopList = MutableStateFlow(PopularMovieList())
-    val statePopList = _statePopList.asStateFlow()
-*/
 
 
 
 
 
-    /*fun fetchMovieData() = viewModelScope.launch {
-        val movieResponse = requestRepository.getMovieList()
-        *//*_stateResult.update { it.copy(backdrop_path = requestRepository.getMovieList().body()!!.results[0].poster_path) }*//*
-        _statePopList.update { it.copy(results = requestRepository.getMovieList().body()!!.results) }
-        Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()}")
-        Log.e("GogaResponse", "${movieResponse.isSuccessful} ${movieResponse.body()!!.results.size}")
-    }*/
+
+
+    fun fetchMovieData(dataForDetailScreen: Result) {
+        _stateResult.update { it.copy(
+            page = dataForDetailScreen.page,
+            adult = dataForDetailScreen.adult,
+            backdrop_path = dataForDetailScreen.backdrop_path,
+            myId = dataForDetailScreen.myId,
+            id = dataForDetailScreen.id,
+            original_language = dataForDetailScreen.original_language,
+            original_title = dataForDetailScreen.original_title,
+            overview = dataForDetailScreen.overview,
+            popularity = dataForDetailScreen.popularity,
+            poster_path = dataForDetailScreen.poster_path,
+            release_date = dataForDetailScreen.release_date,
+            title = dataForDetailScreen.title,
+            video = dataForDetailScreen.video,
+            vote_average = dataForDetailScreen.vote_average,
+            vote_count = dataForDetailScreen.vote_count) }
+
+        Log.e("GogaFetchLink", stateResult.toString())
+    }
+
 
 
 
