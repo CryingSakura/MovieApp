ea90bc3b779686a53a854cccf9579a1fff45438c
Danya Goga <CryingSakura69@gmail.com>
2024-03-06 20:42:38 +0200

    del comment

--- app/src/main/java/com/example/movieapp_try1/screens/fragments/FilmDetailsScreen.kt
+++ app/src/main/java/com/example/movieapp_try1/screens/fragments/FilmDetailsScreen.kt
@@ -35,7 +35,7 @@ fun FilmDetailsScreen(viewModel: MainActivityViewModel){
     val imgUrl = RequestService.IMAGE_URL + movie.poster_path
 
 
-    /*dfgdsfgdsfgdsfgdfgdfgdfgdfgdfg*/
+
 
     Box (modifier = Modifier.fillMaxSize())
     {
