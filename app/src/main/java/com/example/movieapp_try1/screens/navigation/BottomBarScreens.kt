5984c1e0c766b8e44a83368694759a0efe3ec5c2
Danya Goga <CryingSakura69@gmail.com>
2024-03-06 18:19:57 +0200

    fix for navigation

--- app/src/main/java/com/example/movieapp_try1/screens/navigation/BottomBarScreens.kt
+++ app/src/main/java/com/example/movieapp_try1/screens/navigation/BottomBarScreens.kt
@@ -1,14 +1,9 @@
 package com.example.movieapp_try1.screens.navigation
 
-import android.graphics.drawable.Icon
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import com.example.movieapp_try1.R
-
 sealed class BottomBarScreens(
     val route: String
 ) {
     object Blog: BottomBarScreens(route = Graphs.BLOG)
-    object ComingSoon: BottomBarScreens(route = Graphs.BLOG)
-    object FilmList: BottomBarScreens(route = Graphs.BLOG)
+    object ComingSoon: BottomBarScreens(route = Graphs.COMING_SOON)
+    object FilmList: BottomBarScreens(route = Graphs.WATCHLIST)
 }
