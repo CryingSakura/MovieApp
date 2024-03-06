a25f22e6e0275257f77a89df6654b4a21420ede5
Danya Goga <CryingSakura69@gmail.com>
2024-03-06 18:23:54 +0200

    assigned variables to visualize the detailsScreen

--- app/src/main/java/com/example/movieapp_try1/data/model/Result.kt
+++ app/src/main/java/com/example/movieapp_try1/data/model/Result.kt
@@ -1,33 +1,22 @@
 package com.example.movieapp_try1.data.model
 
+import java.io.Serializable
+
 
 data class Result(
-    var page: Int,
-    val adult: Boolean,
-    val backdrop_path: String?,
-    val myId: Int,
-    val id: Int,
-    val original_language: String,
-    val original_title: String,
-    val overview: String,
-    val popularity: Double,
-    val poster_path: String,
-    val release_date: String,
-    val title: String,
-    val video: Boolean,
-    val vote_average: Double,
-    val vote_count: Int
-    /*  val adult: Boolean = false,
-      val backdrop_path: String = "",
-      val id: Int = 0,
-      val original_language: String = "",
-      val original_title: String = "",
-      val overview: String = "",
-      val popularity: Double = 0.0,
-      val poster_path: String = "",
-      val release_date: String = "",
-      val title: String = "",
-      val video: Boolean = false,
-      val vote_average: Double = 0.0,
-      val vote_count: Int = 0*/
-)
+    var page: Int = 0,
+    val adult: Boolean = false,
+    val backdrop_path: String? = null,
+    val myId: Int = 0,
+    val id: Int = 0,
+    val original_language: String = "",
+    val original_title: String = "Originat Test",
+    val overview: String = "When a Delta Force special ops mission goes terribly wrong, Air Force drone pilot Reaper has 48 hours to remedy what has devolved into a wild rescue operation. With no weapons and no communication other than the drone above, the ground mission suddenly becomes a full-scale battle when the team is discovered by the enemy.",
+    val popularity: Double = 0.0,
+    val poster_path: String = "",
+    val release_date: String = "",
+    val title: String = "Test",
+    val video: Boolean = true,
+    val vote_average: Double = 0.0,
+    val vote_count: Int = 0
+) : Serializable
